73d607b8c58b2e551670e96ad44834878dda0866
Bharathipriya Nagarajan <bharathibp1112@gmail.com>
2026-05-31 18:39:26 +0530

    Add logging for database connection string

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ builder.Services.AddCors(options =>
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") 
     ?? builder.Configuration.GetConnectionString("DefaultConnection");
 
+Console.WriteLine($"Connection string: {connectionString}");
+
 builder.Services.AddDbContext<TodoDbContext>(options =>
     options.UseSqlServer(connectionString));
 
